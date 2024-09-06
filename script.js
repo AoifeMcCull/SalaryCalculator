7c587700fbfda531507b027ab76f2059f2b70b7b
Jamie McCullough <92225779+KeeganMcCullough@users.noreply.github.com>
2024-09-06 14:29:11 -0500

    Added forms and submit button. Added skeleton of employees table. Added TODOS and function names.

---
+++ script.js
@@ -0,0 +1,20 @@
+//TODO: add employee function
+//TODO: submit button clears inputs
+//TODO: over-budget css to the footer if monthly cost > 20,000
+//TODO: delete button removes employee from the document
+
+function submitEmployee(){
+    //TODO:
+}
+
+function updateTotal(){
+    //TODO:
+}
+
+function checkBudget(){
+    //TODO:
+}
+
+function deleteEmployee(){
+    //TODO:
+}
